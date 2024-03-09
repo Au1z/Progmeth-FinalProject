23bf82d672c4a3f5ca5e24fb1a46e80755815920
Auiz <nopphawit47374@gmail.com>
2024-03-09 21:13:07 +0700

    add method in Main

--- src/Main.java
+++ src/Main.java
@@ -12,34 +12,30 @@ public class Main {
             areas.add(new Area());
         }
         while (player1.getHp() != 0 && player2.getHp() != 0) {
-            dice.randomFaceValue();
-            player1.setPosition(player1.getPosition() + dice.getFaceValue());
-            System.out.println("player1: " + player1.getHp() + " position: " + player1.getPosition());
-            if (areas.get(player1.getPosition()).getOwner().getName().isEmpty()) {
-                areas.get(player1.getPosition()).setLevel(1);
-                areas.get(player1.getPosition()).setOwner(player1);
-            } else if (areas.get(player1.getPosition()).getOwner().equals(player1)) {
-                areas.get(player1.getPosition()).setLevel(areas.get(player1.getPosition()).getLevel() + 1);
-            } else {
-                player1.setHp(player1.getHp() - areas.get(player1.getPosition()).getLevel());
+            play(player1, areas);
+            if (player1.getHp() == 0) {
+                break;
             }
-            System.out.println("player1: " + player1.getHp() + " position: " + player1.getPosition());
-            Scanner scanner = new Scanner(System.in);
-            scanner.nextLine();
-            dice.randomFaceValue();
-            player2.setPosition(player2.getPosition() + dice.getFaceValue());
-            System.out.println("player2: " + player2.getHp() + " position: " + player2.getPosition());
-            if (areas.get(player2.getPosition()).getOwner().getName().isEmpty()) {
-                areas.get(player2.getPosition()).setLevel(1);
-                areas.get(player2.getPosition()).setOwner(player2);
-            } else if (areas.get(player2.getPosition()).getOwner().equals(player2)) {
-                areas.get(player2.getPosition()).setLevel(areas.get(player2.getPosition()).getLevel() + 1);
-            } else {
-                player2.setHp(player2.getHp() - areas.get(player2.getPosition()).getLevel());
-            }
-            System.out.println("player2: " + player2.getHp() + " position: " + player2.getPosition());
-            scanner.nextLine();
+            play(player2, areas);
         }
-        System.out.println("Game End");
+        System.out.println(player1.getHp() == 0 ? "player2 win!!" : "player1 win!!");
+    }
+
+    public static void play(Player player, ArrayList<Area> areas) {
+        Dice dice = new Dice();
+        dice.randomFaceValue();
+        player.setPosition(player.getPosition() + dice.getFaceValue());
+        System.out.println(player.getName() + ": "  + player.getHp() + " position: " + player.getPosition());
+        if (areas.get(player.getPosition()).getOwner().getName().isEmpty()) {
+            areas.get(player.getPosition()).setLevel(1);
+            areas.get(player.getPosition()).setOwner(player);
+        } else if (areas.get(player.getPosition()).getOwner().equals(player)) {
+            areas.get(player.getPosition()).setLevel(areas.get(player.getPosition()).getLevel() + 1);
+        } else {
+            player.setHp(player.getHp() - areas.get(player.getPosition()).getLevel());
+        }
+        System.out.println(player.getName() + ": " + player.getHp() + " position: " + player.getPosition());
+        Scanner scanner = new Scanner(System.in);
+        scanner.nextLine();
     }
 }
