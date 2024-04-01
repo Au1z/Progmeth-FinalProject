82b3f9325b0f92cfaf942d25995866917940351e
NattawitMana <6633066721@student.chula.ac.th>
2024-04-01 10:44:08 +0700

    buy area

--- src/Main.java
+++ src/Main.java
@@ -26,26 +26,61 @@ public class Main {
         Dice dice2 = new Dice();
         dice2.randomFaceValue();
 
-        System.out.println(player.getPosition());
-        System.out.println("Dice1 is " + dice1.getFaceValue());
-        System.out.println("Dice2 is " + dice2.getFaceValue());
+        System.out.println("<< " + player.getName() + " Turn >>");
+        System.out.println("Now position is " + player.getPosition());
+        System.out.println("Dice1 is " + dice1.getFaceValue() + " and Dice2 is " + dice2.getFaceValue());
 
         int totalMove = dice1.getFaceValue() + dice2.getFaceValue();
         player.setPosition(player.getPosition() + totalMove);
 
         System.out.println("Now player move " + totalMove + " position");
-        System.out.println(player.getName() + ": "  + player.getHp() + " position: " + player.getPosition());
+        System.out.println(player.getName() + ": " + player.getHp() + " position: " + player.getPosition());
 
-        if (areas.get(player.getPosition()).getOwner().getName().isEmpty()) {
-            areas.get(player.getPosition()).setLevel(1);
-            areas.get(player.getPosition()).setOwner(player);
-        } else if (areas.get(player.getPosition()).getOwner().equals(player)) {
-            areas.get(player.getPosition()).setLevel(areas.get(player.getPosition()).getLevel() + 1);
-        } else {
-            player.setHp(player.getHp() - areas.get(player.getPosition()).getLevel());
-        }
+        existArea(player, areas);
+
         System.out.println(player.getName() + ": " + player.getHp() + " position: " + player.getPosition());
         Scanner scanner = new Scanner(System.in);
         scanner.nextLine();
     }
+
+    private static void existArea(Player player, ArrayList<Area> areas) {
+        if (areas.get(player.getPosition()).getOwner().getName().isEmpty()) {
+            System.out.println("Do you want to buy this Area? (Y/N)");
+            Scanner scanner = new Scanner(System.in);
+            String ans = scanner.nextLine();
+            switch (ans) {
+                case "Y":
+                    if (player.getHp() - 1 <= 0) {
+                        System.out.println("Cannot buy this area");
+                    } else {
+                        player.setHp(player.getHp() - 1);
+                        areas.get(player.getPosition()).setLevel(1);
+                        areas.get(player.getPosition()).setOwner(player);
+                    }
+                    break;
+                case "N":
+                    break;
+            }
+        } else if (areas.get(player.getPosition()).getOwner().equals(player)) {
+            System.out.println("Do you want to upgrade this area? (Y/N)");
+            Scanner scanner = new Scanner(System.in);
+            String ans = scanner.nextLine();
+            switch (ans) {
+                case "Y":
+                    if (player.getHp() - areas.get(player.getPosition()).getLevel() <= 0) {
+                        System.out.println("Cannot upgrade this area");
+                    } else {
+                        areas.get(player.getPosition()).setLevel(areas.get(player.getPosition()).getLevel() + 1);
+                        player.setHp(player.getHp() - 1);
+                    }
+                    break;
+                case "N":
+                    break;
+            }
+        } else {
+            System.out.println("You lose " + areas.get(player.getPosition()).getLevel() + " hp");
+            player.setHp(player.getHp() - areas.get(player.getPosition()).getLevel());
+        }
+    }
+
 }
